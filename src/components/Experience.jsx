dd656c2eb2f3e6d86f5c3b925ab372228d7df2d4
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-11-19 23:45:11 -0500

    Added the work experience header and wrapped it to SectionWrapper

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { SectionWrapper } from '../hoc'
+import { motion } from 'framer-motion'
+import { styles } from '../style'
+import { textVariant } from '../utils/motion'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work")

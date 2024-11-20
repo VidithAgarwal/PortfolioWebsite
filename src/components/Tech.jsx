bdb478687b11e2643daba01e703a009b20845a81
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-11-20 03:47:50 -0500

    Styled the skills section using Float and canvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../style'
+import { SectionWrapper } from '../hoc'
+import { fadeIn, textVariant } from '../utils/motion'
+import { technologies } from '../constants'
+import {BallCanvas} from './canvas'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((tech, index) => (
+        <div className='w-28 h-28' key={tech.name}>
+          <BallCanvas icon = {tech.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "tech")

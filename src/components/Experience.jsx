5f59c4ee6e50ecc5c59d128e3157ec085aa22db0
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-11-20 03:01:32 -0500

    Styled the work experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,26 @@ import { SectionWrapper } from '../hoc'
 import { motion } from 'framer-motion'
 import { styles } from '../style'
 import { textVariant } from '../utils/motion'
+import 'react-vertical-timeline-component/style.min.css'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import { experiences } from '../constants'
+const ExperienceCard = ({ exp }) => {
+  return (
+    <VerticalTimelineElement 
+    contentStyle={{ background: '#1d1836', color: '#fff' }} contentArrowStyle={{ borderRight: '7px solid  #232631' }} date={exp.date} iconStyle={{ background: exp.iconBg }} icon={<div className='flex justify-center items-center w-full h-full'><img src={exp.icon} alt={exp.company_name} className='w-[60%] h-[60%] object-contain' /></div>}
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{exp.title}</h3>
+        <p className='text-secondary text-[16px] font-semibold' style={{margin: 0}}>{exp.company_name}</p>
+        <ul className='mt-5 list-disc ml-5 space-y-2'>
+          {exp.points.map((point, index) => (
+            <li key={`experience-point-${index}`} className='text-white-100 text-[14px] pl-1 tracking-wider'>{point}</li>
+          ))}
+        </ul>
+      </div>
+    </VerticalTimelineElement>
+  )
+}
 
 const Experience = () => {
   return (
@@ -11,6 +31,13 @@ const Experience = () => {
         <p className={styles.sectionSubText}>What I have done so far</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
+      <div className='mt-20 flex flex-col gap-10'>
+        <VerticalTimeline>
+          {experiences.map((exp, index) => (
+            <ExperienceCard key={index} exp={exp} />
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   )
 }

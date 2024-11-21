61337f8c21e8371b12cf3a845582110bb5c97a05
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-11-20 20:07:03 -0500

    Styled the project section and added cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
  import { BrowserRouter } from  "react-router-dom";
 
-  import {About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works} from "./components";
+  import {About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works} from "./components";
  
  const App = () => {
 
@@ -15,7 +15,6 @@
         <Experience/>
         <Tech/>
         <Works/>
-        <Feedbacks/>
         <div className="relative z-0">
           <Contact/>
           <StarsCanvas />
